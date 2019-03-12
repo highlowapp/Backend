284eca280c4903f18b509f5351b051253b124658
Brayden Morris <34358864+R3d3MpT10n101@users.noreply.github.com>
2019-03-11 18:50:13 -0700

    Update python_with_mysql_test.py

--- python_with_mysql_test.py
+++ python_with_mysql_test.py
@@ -3,20 +3,21 @@ import MySQLdb
 
 db = MySQLdb.connect(host="localhost",  # Put in your host name 
                      user="root",       # Put in your username default is root
-                     passwd="your_password",     # Put in your password for MySQL
-                     db="database_name")   # Put in the name of the database you want to work on 
+                     passwd="your password",     # Put in your password for MySQL
+                     db="high_low_db")   # Put in the name of the database you want to work on 
  
 # Create a Cursor object to execute queries.
 cur = db.cursor()
 
 #Get the values for the table
-inputname = raw_input("What is your name? ")
+inputfirstname = raw_input("What is your first name? ")
+inputlastname = raw_input("What is your last name? ")
 inputemail = raw_input("What is your email? ")
 inputpassword = raw_input("What is your password? ")
 
 
 # Execute the MySQL command to insert the values
-cur.execute("INSERT INTO database_name (name, email, password) VALUES ('{}', '{}', '{}');".format(inputname, inputemail, inputpassword))
+cur.execute("INSERT INTO high_low_db (first_name, last_name, email, password) VALUES ('{}', '{}', '{}', '{}');".format(inputfirstname, inputlastname, inputemail, inputpassword))
 
 
 db.commit()
